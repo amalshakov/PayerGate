e29cc0969e31cfeff6701f42d3939889b0a4a404
Alexander Malshakov <89681115653@mail.ru>
2024-07-19 11:27:25 +0300

    Change PhotoSerializer, Add PhotoUploadSerializer

--- accounting_for_pets/api/v1/serializers.py
+++ accounting_for_pets/api/v1/serializers.py
@@ -6,9 +6,15 @@ from pets.models import Pet, Photo
 class PhotoSerializer(serializers.ModelSerializer):
     """."""
 
+    url = serializers.SerializerMethodField()
+
     class Meta:
         model = Photo
         fields = ["id", "url"]
+
+    def get_url(self, obj):
+        request = self.context.get("request")
+        return obj.get_full_url(request) if request else obj.file.url
 
 
 class PetSerializer(serializers.ModelSerializer):
@@ -20,13 +26,37 @@ class PetSerializer(serializers.ModelSerializer):
         model = Pet
         fields = ["id", "name", "age", "type", "photos", "created_at"]
 
+    # def validate_type(self, value):
+    #     if value not in ["cat", "dog"]:
+    #         raise serializers.ValidationError(
+    #             "Type must be either 'cat' or 'dog'"
+    #         )
+    #     return value
 
-class PetCreateSerializer(serializers.ModelSerializer):
+
+# class PetCreateSerializer(serializers.ModelSerializer):
+#     """."""
+
+#     class Meta:
+#         model = Pet
+#         fields = ["id", "name", "age", "type"]
+
+#     def create(self, validated_data):
+#         return Pet.objects.create(**validated_data)
+
+
+class PhotoUploadSerializer(serializers.ModelSerializer):
     """."""
 
+    url = serializers.SerializerMethodField()
+
     class Meta:
-        model = Pet
-        fields = ["id", "name", "age", "type"]
+        model = Photo
+        fields = ["id", "file", "url"]
+
+    def get_url(self, obj):
+        request = self.context.get("request")
+        return obj.get_full_url(request) if request else obj.file.url
 
     def create(self, validated_data):
-        return Pet.objects.create(**validated_data)
+        return Photo.objects.create(**validated_data)
