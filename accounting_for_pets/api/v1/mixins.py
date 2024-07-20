9a134246b3d8b1e0061a4288fd7993ad12bd33da
Alexander Malshakov <89681115653@mail.ru>
2024-07-20 19:44:08 +0300

    Change mixins comments

--- accounting_for_pets/api/v1/mixins.py
+++ accounting_for_pets/api/v1/mixins.py
@@ -3,18 +3,18 @@ from pets.models import Photo
 
 class PhotoURLMixin:
     """
-    Миксин для добавления метода получения полного URL файла фото.
+    Миксин для добавления метода получения полного URL файла (фотографии).
     """
 
     def get_url(self, obj: Photo) -> str:
         """
-        Возвращает полный URL файла фото.
+        Возвращает полный URL файла (фотографии).
 
         Аргументы:
             obj (Photo): Экземпляр модели Photo.
 
         Возвращает:
-            str: Полный URL файла фото.
+            str: Полный URL файла (фотографии).
         """
         request = self.context.get("request")
         return obj.get_full_url(request) if request else obj.file.url
